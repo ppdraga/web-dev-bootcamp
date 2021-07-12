ff49d584c6500000723aeb56bbb78ecc5c5b8458
Yaroslav <ppdraga@gmail.com>
2021-07-12 22:20:25 +0300

    i1

--- calc/calc.js
+++ calc/calc.js
@@ -3,11 +3,11 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hi there!");
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/contact', (req, res) => {
-    res.send("Contact page!");
+app.post('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(8085, () => {
