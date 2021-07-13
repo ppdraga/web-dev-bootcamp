d2bbd95b2677b4fdc2254c12fb50919b2c526438
Yaroslav <ppdraga@gmail.com>
2021-07-13 21:58:05 +0300

    w1

---
+++ WeatherProject/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Server is up and running!");
+});
+
+app.listen(8085, () => {
+    console.log("Listening on port 8085.")
+});
