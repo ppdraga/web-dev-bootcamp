a986cbba24df14d61b99329d9ed7c213b67525ec
Yaroslav <ppdraga@gmail.com>
2021-07-21 22:04:41 +0300

    api add email to list

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 //jshint esversion: 6
 
 const express = require('express');
+const request = require('request')
 const https = require('https');
 const bodyParser = require('body-parser')
 
@@ -13,14 +14,48 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
     // res.sendFile(__dirname + "/signup.html");
     console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us6.api.mailchimp.com/3.0/lists/2f65e3ad09";
+    const options = {
+        method: "POST",
+        auth: "ppdraga:36c174045e9d9b01e8f12f82800b0027-us6"
+    }
+    const request = https.request(url, options, (response) => {
+        console.log(response.statusCode);
+        response.on("data", (data) => {
+            const data2 = JSON.parse(data);
+            console.log(data2);
+        });
+    });
+    request.write(jsonData);
+    request.end();
+
 })
 
 app.listen(8085, () => {
     console.log("Listening on port 8085.");
 });
 
+// api key:
+// 36c174045e9d9b01e8f12f82800b0027-us6
+
+// List id
+// 2f65e3ad09
