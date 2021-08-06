e413b8201363190053ff02247fa9233581088151
Yaroslav <ppdraga@gmail.com>
2021-08-06 20:51:12 +0300

    todo list v2 commit 1

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -2,31 +2,62 @@
 
 const express = require('express');
 const https = require('https');
-const bodyParser = require('body-parser')
-const date = require(__dirname + "/date.js")
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var items = ["action 1", "action 2", "action 3"];
+mongoose.connect("mongodb://mongo:mongo@localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// login to mongo shell
+// use todolistDB
+// db.createUser({user: 'mongo', pwd: 'mongo', roles: [ { role: 'readWrite', db: 'todolistDB'} ]});
+mongoose.set('useFindAndModify', false);
+
+const itemSchema = {
+    name: String
+  };
+const Item = mongoose.model("Item", itemSchema);
+
+var items = [];
 var workItems = [];
 
 app.get("/", (req, res) => {
-
-    res.render('list', {listTitle: date.getDate(), newListItems: items});
+    Item.find({}, function(err, items){
+        res.render("list", {
+            listTitle: date.getDate(), 
+            newListItems: items
+        });
+      });
 })
 
 app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    if (req.body.list === "Work") {
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
+    const item = new Item ({
+        name: req.body.newItem,
+    });
+    item.save(function(err){
+        if (!err) {
+            res.redirect("/");
+        }
+    });
+
+    // if (req.body.list === "Work") {
+    //     workItems.push(item);
+    //     res.redirect("/work");
+    // } else {
+    //     items.push(item);
+    //     res.redirect("/");
+    // }
+});
+
+app.post("/delete", (req, res) => {
+    let itemID = req.body.checkbox;
+    Item.findByIdAndRemove(itemID, function(err){ 
         res.redirect("/");
-    }
+    });
 });
 
 app.get("/work", (req, res) => {
