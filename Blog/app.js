21549b302c126b7bec3b59235a29eda2aedeabc9
Yaroslav <ppdraga@gmail.com>
2021-08-06 12:03:38 +0300

    blog 2

--- Blog/app.js
+++ Blog/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -22,14 +23,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/posts/:postTitle", (req, res) => {
-  const requestedTitle = req.params.postTitle;
+  console.log("posts view");
+  const requestedTitle = _.lowerCase(req.params.postTitle);
+  let isPostFound = false;
   posts.forEach( (post) => {
-    if (post.title === requestedTitle) {
-      console.log("Match found!");
+    const storedTitle = _.lowerCase(post.title);
+    console.log(storedTitle, requestedTitle);
+    if (storedTitle === requestedTitle) {
+      console.log("match");
+      res.render('post', {post: post});
+      isPostFound = true;
       return;
     }
+    console.log("not match");
   });
-  res.render('home', {startingContent: homeStartingContent, posts: posts});
+  if (isPostFound === false) {
+    console.log("home page render");
+    res.render('home', {startingContent: homeStartingContent, posts: posts});
+  }
 });
 
 app.get("/about", (req, res) => {
@@ -56,6 +67,7 @@ app.post("/compose", (req, res) => {
     title: req.body.postTitle,
     content: postBody
   });
+  // console.log(posts);
   res.redirect("/");
 });
 
