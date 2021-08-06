fd0e85cac927379dee04264c124a5757c5bfa960
Yaroslav <ppdraga@gmail.com>
2021-08-06 16:04:46 +0300

    add mongo

--- Blog/app.js
+++ Blog/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,31 +17,53 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://mongo:mongo@localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true});
+// login to mongo shell
+// use blogDB
+// db.createUser({user: 'mongo', pwd: 'mongo', roles: [ { role: 'readWrite', db: 'blogDB'} ]});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+const Post = mongoose.model("Post", postSchema);
+
 const posts = [];
 
 app.get("/", (req, res) => {
-  res.render('home', {startingContent: homeStartingContent, posts: posts});
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+  // res.render('home', {startingContent: homeStartingContent, posts: posts});
 });
 
-app.get("/posts/:postTitle", (req, res) => {
+app.get("/posts/:postId", (req, res) => {
   console.log("posts view");
-  const requestedTitle = _.lowerCase(req.params.postTitle);
-  let isPostFound = false;
-  posts.forEach( (post) => {
-    const storedTitle = _.lowerCase(post.title);
-    console.log(storedTitle, requestedTitle);
-    if (storedTitle === requestedTitle) {
-      console.log("match");
-      res.render('post', {post: post});
-      isPostFound = true;
-      return;
-    }
-    console.log("not match");
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id: requestedPostId}, function(err, post){ 
+    console.log(post)
+    res.render('post', {post: post});
   });
-  if (isPostFound === false) {
-    console.log("home page render");
-    res.render('home', {startingContent: homeStartingContent, posts: posts});
-  }
+  // const requestedTitle = _.lowerCase(req.params.postTitle);
+  // let isPostFound = false;
+  // posts.forEach( (post) => {
+  //   const storedTitle = _.lowerCase(post.title);
+  //   console.log(storedTitle, requestedTitle);
+  //   if (storedTitle === requestedTitle) {
+  //     console.log("match");
+  //     res.render('post', {post: post});
+  //     isPostFound = true;
+  //     return;
+  //   }
+  //   console.log("not match");
+  // });
+  // if (isPostFound === false) {
+  //   console.log("home page render");
+  //   res.render('home', {startingContent: homeStartingContent, posts: posts});
+  // }
 });
 
 app.get("/about", (req, res) => {
@@ -67,8 +90,19 @@ app.post("/compose", (req, res) => {
     title: req.body.postTitle,
     content: postBody
   });
+
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+  post.save(function(err){
+    if (!err) {
+      res.redirect("/");
+    }
+  });
   // console.log(posts);
-  res.redirect("/");
+  // res.redirect("/");
 });
 
 
